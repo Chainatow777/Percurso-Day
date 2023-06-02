f95be030359dac950d56d3ffd49f00d77cc82bff
Samuell Morgesntern de Souza <107222555+Chainatow777@users.noreply.github.com>
2023-06-02 17:08:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,113 @@ var nomesCliente = [];
 var horariosDeixado = [];
 var numerosDeixado = [];
 var numerosColetado = [];
+document.addEventListener("DOMContentLoaded", function() {
+    var placaInput = document.getElementById("placa");
+    var placas = [
+        "ABC123",
+        "DEF456",
+        "GHI789"
+        // Adicione outras placas à lista conforme necessário
+    ];
+
+    new Autocomplete(placaInput, placas);
+
+    function Autocomplete(input, options) {
+        var currentFocus;
+
+        input.addEventListener("input", function() {
+            var inputText = this.value;
+            var autocompleteList = options.filter(function(option) {
+                return option.startsWith(inputText.toUpperCase());
+            });
+
+            closeAllLists();
+
+            if (inputText.length > 0 && autocompleteList.length > 0) {
+                currentFocus = -1;
+                var autocompleteContainer = createAutocompleteContainer();
+
+                autocompleteList.forEach(function(option) {
+                    var autocompleteOption = createAutocompleteOption(option);
+                    autocompleteContainer.appendChild(autocompleteOption);
+                });
+            }
+        });
+
+        input.addEventListener("keydown", function(e) {
+            var autocompleteItems = document.getElementById(this.id + "-autocomplete-list");
+
+            if (!autocompleteItems) {
+                return;
+            }
+
+            autocompleteItems = autocompleteItems.getElementsByTagName("div");
+
+            if (e.keyCode === 40) { // Seta para baixo
+                currentFocus++;
+                addActive(autocompleteItems);
+            } else if (e.keyCode === 38) { // Seta para cima
+                currentFocus--;
+                addActive(autocompleteItems);
+            } else if (e.keyCode === 13) { // Enter
+                e.preventDefault();
+                if (currentFocus > -1) {
+                    autocompleteItems[currentFocus].click();
+                }
+            }
+        });
+
+        function createAutocompleteContainer() {
+            var autocompleteContainer = document.createElement("div");
+            autocompleteContainer.setAttribute("id", input.id + "-autocomplete-list");
+            autocompleteContainer.setAttribute("class", "autocomplete-items");
+            autocompleteContainer.style.backgroundColor = "#FFCCCC"; // Defina a cor de fundo desejada
+            input.parentNode.appendChild(autocompleteContainer);
+            return autocompleteContainer;
+        }        
+
+        function createAutocompleteOption(option) {
+            var autocompleteOption = document.createElement("div");
+            autocompleteOption.textContent = option;
+            autocompleteOption.addEventListener("click", function() {
+                input.value = this.textContent;
+                closeAllLists();
+            });
+            return autocompleteOption;
+        }
+
+        function addActive(autocompleteItems) {
+            removeActive(autocompleteItems);
+
+            if (currentFocus >= autocompleteItems.length) {
+                currentFocus = 0;
+            }
+
+            if (currentFocus < 0) {
+                currentFocus = autocompleteItems.length - 1;
+            }
+
+            autocompleteItems[currentFocus].classList.add("autocomplete-active");
+        }
+
+        function removeActive(autocompleteItems) {
+            Array.from(autocompleteItems).forEach(function(item) {
+                item.classList.remove("autocomplete-active");
+            });
+        }
+
+        function closeAllLists() {
+            var autocompleteItems = document.getElementsByClassName("autocomplete-items");
+            Array.from(autocompleteItems).forEach(function(item) {
+                item.parentNode.removeChild(item);
+            });
+        }
+
+        document.addEventListener("click", function(e) {
+            closeAllLists();
+        });
+    }
+});
 
 function adicionarLinha() {
     var divInformacoes = document.getElementById('informacoes');
@@ -46,30 +153,29 @@ function removerLinha() {
 }
 
 function verificarFormulario() {
-    var horariosChegada = document.getElementsByName('horario-chegada[]');
-    var nomesCliente = document.getElementsByName('nome-cliente[]');
-    var horariosDeixado = document.getElementsByName('horarios-deixado[]');
-    var numerosDeixado = document.getElementsByName('numero-deixado[]');
-    var numerosColetado = document.getElementsByName('numero-coletado[]');
+    var campos = document.querySelectorAll('input[required], select[required], textarea[required]');
 
-    var isFormFilled = verificarFormularioPreenchido(horariosChegada, nomesCliente, horariosDeixado, numerosDeixado, numerosColetado);
-
-    if (isFormFilled) {
-        imprimirTabela();
-    } else {
-        alert('Preencha todos os campos do formulário antes de enviar.');
-    }
-}
-
-function verificarFormularioPreenchido(...campos) {
     for (var i = 0; i < campos.length; i++) {
-        for (var j = 0; j < campos[i].length; j++) {
-            if (campos[i][j].value.trim() === '') {
-                return false;
-            }
+        if (campos[i].value.trim() === '') {
+            Swal.fire({
+                title: 'Campos Incompletos',
+                icon: 'warning',
+                text: 'Preencha todos os campos do formulário antes de enviar.',
+            });
+
+            campos[i].style.border = '3px solid #dc3545';
+
+            campos[i].addEventListener('input', function () {
+                if (this.value.trim() !== '') {
+                    this.style.border = '';
+                }
+            });
+
+            return;
         }
     }
-    return true;
+
+    imprimirTabela();
 }
 
 function enviarFormulario() {
@@ -96,11 +202,11 @@ function adicionarDadoTabela(valor) {
 }
 
 function imprimirTabela() {
-    horariosChegada = document.getElementsByName('horario-chegada[]');
-    nomesCliente = document.getElementsByName('nome-cliente[]');
-    horariosDeixado = document.getElementsByName('horarios-deixado[]');
-    numerosDeixado = document.getElementsByName('numero-deixado[]');
-    numerosColetado = document.getElementsByName('numero-coletado[]');
+    var horariosChegada = document.getElementsByName('horario-chegada[]');
+    var nomesCliente = document.getElementsByName('nome-cliente[]');
+    var horariosDeixado = document.getElementsByName('horarios-deixado[]');
+    var numerosDeixado = document.getElementsByName('numero-deixado[]');
+    var numerosColetado = document.getElementsByName('numero-coletado[]');
 
     var tabelaCorpo = document.getElementById('tabelaCorpo');
     tabelaCorpo.innerHTML = '';
@@ -131,39 +237,67 @@ function imprimirTabela() {
         tabelaCorpo.appendChild(linhaHTML);
     }
 
-    var totalColetas = dados.length - 1; // Excluindo o cabeçalho
-    var mediaColetas = totalColetas / 1; // Dividido por 1 (um) como mencionado na pergunta
+    var totalColetas = dados.length - 1;
+    var mediaColetas = totalColetas / 1;
 
-    // Obtém os valores dos campos de quilometragem
     var kmInicial = parseFloat(document.getElementById('km-inicial').value);
     var kmFinal = parseFloat(document.getElementById('km-final').value);
 
-    // Calcula a quilometragem percorrida
     var quilometragemPercorrida = kmFinal - kmInicial;
 
-    var mensagem = 'Total de coletas: ' + totalColetas + '\n';
-    mensagem += 'Média de coletas: ' + mediaColetas + '\n';
-    mensagem += 'Quilometragem percorrida: ' + quilometragemPercorrida + '\n';
-    mensagem += 'Deseja baixar o arquivo em Excel?';
+    var mensagem = '<p>Total de coletas: ' + totalColetas + '</p>';
+    mensagem += '<p>Média de coletas: ' + mediaColetas + '</p>';
+    mensagem += '<p>Quilometragem percorrida: ' + quilometragemPercorrida + '</p>';
+    mensagem += '<p><strong>Deseja baixar o arquivo em Excel?</strong></p>';
 
-    var downloadExcel = confirm(mensagem);
+    Swal.fire({
+        title: 'Alerta',
+        html: mensagem,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Não',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        reverseButtons: true,
+        customClass: {
+            container: 'my-sweetalert-container',
+            content: 'my-sweetalert-content',
+        },
+    }).then(function (result) {
+        if (result.isConfirmed) {
+            console.log('Clique em Sim');
+            var downloadExcel = true;
 
-    var isFormFilled = verificarFormularioPreenchido(horariosChegada, nomesCliente, horariosDeixado, numerosDeixado, numerosColetado);
-    if (isFormFilled && downloadExcel) {
-        var wb = XLSX.utils.book_new();
-        var ws = XLSX.utils.aoa_to_sheet(dados);
+            if (downloadExcel) {
+                var wb = XLSX.utils.book_new();
+                var ws = XLSX.utils.aoa_to_sheet(dados);
 
-        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-        XLSX.writeFile(wb, 'percurso_diario.xlsx');
-    }
+                XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+                XLSX.writeFile(wb, 'percurso_diario.xlsx');
 
-    exibirInformacoesCalendario();
+                Swal.fire({
+                    title: 'Download do Excel',
+                    icon: 'success',
+                    text: 'O arquivo Excel foi baixado com sucesso!',
+                });
+            }
+        } else {
+            // Ação para o botão "Não"
+            console.log('Clique em Não');
+        }
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
-    });
-    calendar.render();
-});
+// Seletor para os campos de entrada
+// var camposInput = document.querySelectorAll('input[type="text"], input[type="time"], input[type="number"]');
+
+// camposInput.forEach(function (campo) {
+//     var label = campo.previousElementSibling.getAttribute('data-tippy-content') || campo.previousElementSibling.innerText;
+//     tippy(campo, {
+//         content: label,
+//         theme: 'custom',
+//         allowHTML: true,
+//     });
+// });
+
